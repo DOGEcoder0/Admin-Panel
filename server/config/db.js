2e015cb063ed4567f79480a10d3ad007347de1af
Dharmendra mahala <87034047+DOGEcoder0@users.noreply.github.com>
2024-07-03 23:25:03 +0530

    Update db.js

--- server/config/db.js
+++ server/config/db.js
@@ -2,7 +2,7 @@ const { default: mongoose } = require("mongoose");
 
 const connectDB = async () => {
     try {
-        await mongoose.connect('mongodb+srv://Dhar2003:xQnX1jESNh5W6YKA@cluster0.vf3vs0m.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
+        await mongoose.connect(`${process.env.MONGO_URI}`);
         console.log('MongoDB connected');
     } catch (error) {
         console.error(error.message);
